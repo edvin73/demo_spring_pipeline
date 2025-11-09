ed35edc663137e29ded37d68d34d105108204fab
Edvin <e.tuzlakovic@gmail.com>
2025-11-09 21:41:41 +0100

    Add home with name

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/home")
@@ -11,6 +14,11 @@ public class HomeController {
 	@GetMapping("/hello")
 	public String index() {
 		return "Welcome to the Home Page!";
+	}
+	
+	@GetMapping("/hello/{name}")
+	public String helloWithName(@PathVariable String name) {
+		return "Welcome to the Home Page + " + name + "!";
 	}
 
 }
